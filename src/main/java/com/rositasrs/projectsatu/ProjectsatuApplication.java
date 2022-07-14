9f1ffab241a8976ab10db0ee5684781a63cef977
Rosita120505 <107414498+Rosita120505@users.noreply.github.com>
2022-07-14 20:26:21 +0700

    Initial commit

---
+++ src/main/java/com/rositasrs/projectsatu/ProjectsatuApplication.java
@@ -0,0 +1,13 @@
+package com.rositasrs.projectsatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsatuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rositasrs/projectsatu/ProjectsatuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rositasrs.projectsatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsatuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
