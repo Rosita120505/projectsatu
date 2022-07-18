5b9dac302c67c086e8097659ddcf308f23ed1fba
Nurul Fikriyatil Aniqoh <nurulfikrianiqoh@gmail.com>
2022-07-18 16:22:20 +0700

    nambah kelas logindto

--- src/main/java/com/rositasrs/projectsatu/model/LoginDto.java
+++ src/main/java/com/rositasrs/projectsatu/model/LoginDto.java
@@ -1,4 +1,22 @@
 package com.rositasrs.projectsatu.model;
 
 public class LoginDto {
+    private String uname;
+    private String pass;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
